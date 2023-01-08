4184b3db9791163ff606f43222c016b66a5cd133
Mike Breault <mbreault@users.noreply.github.com>
2023-01-08 13:25:12 -0600

    gitignore fixed

--- my_app/target/debug/deps/my_app-ffbc97d1e462d44a.d
+++
@@ -1,5 +0,0 @@
-/home/parallels/git/mbreault/actix-rust/my_app/target/debug/deps/my_app-ffbc97d1e462d44a: src/main.rs
-
-/home/parallels/git/mbreault/actix-rust/my_app/target/debug/deps/my_app-ffbc97d1e462d44a.d: src/main.rs
-
-src/main.rs:
--- my_app/target/debug/my_app.d
+++
@@ -1,1 +0,0 @@
-/home/parallels/git/mbreault/actix-rust/my_app/target/debug/my_app: /home/parallels/git/mbreault/actix-rust/my_app/src/main.rs
